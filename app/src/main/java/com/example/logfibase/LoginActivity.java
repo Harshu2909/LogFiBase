12c50b7b7f4302f53edd15b51652d6094dd08a89
Harshu <harshadmsorathiya@gmail.com>
2021-06-08 18:33:28 +0530

    connect to firebase using Gmail/pwd

---
+++ app/src/main/java/com/example/logfibase/LoginActivity.java
@@ -0,0 +1,112 @@
+package com.example.logfibase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.net.Inet4Address;
+
+public class LoginActivity extends AppCompatActivity {
+    public EditText emailId,password;
+    Button btnSignIn;
+    TextView tvSignUp;
+    FirebaseAuth mfirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mfirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.editText);
+        password = findViewById(R.id.editText2);
+        btnSignIn = findViewById(R.id.button2);
+        tvSignUp = findViewById(R.id.textView);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull @NotNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mfirebaseAuth.getCurrentUser();
+                if (mFirebaseUser !=null){
+                    Toast.makeText(LoginActivity.this,"You are Logged In",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast.makeText(LoginActivity.this,"Please Login",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email=emailId.getText().toString();
+                String pwd=password.getText().toString();
+                if (email.isEmpty())
+                {
+                    emailId.setError("Please enter email id");
+                    emailId.requestFocus();
+                }
+                else if(pwd.isEmpty())
+                {
+                    password.setError("PLaease enter your Password");
+                    password.requestFocus();
+                }
+                else if (email.isEmpty() && pwd.isEmpty())
+                {
+                    Toast.makeText(LoginActivity.this,"Fields are Empty!",Toast.LENGTH_SHORT).show();
+                }
+                else if (!(email.isEmpty() && pwd.isEmpty()))
+                {
+                    mfirebaseAuth.signInWithEmailAndPassword(email,pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this,"Login Error,Please Login Agian !",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Intent intToHome= new Intent(LoginActivity.this,HomeActivity.class);
+                                startActivity(intToHome);
+                            }
+                        }
+                    });
+
+                }
+                else {
+                    Toast.makeText(LoginActivity.this,"Error Occured!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intSignUp = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intSignUp);
+             }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mfirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+}
